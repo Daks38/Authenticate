17cca4bec415e30739318082cc6421369ef6a48b
DAKS54 <sylvain.diallo@epitech.eu>
2026-02-19 18:22:30 +0000

    [Update] server1 server and swagger

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 //Routes
 //Register
-app.use("/register", registerRouter,swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use("/register", registerRouter)
 
 //Login
 app.use("/login", loginRouter)
